14acf452f4d1a36d354cec0e95b184273631bc84
vlad980000 <vlad980000@mail.ru>
2022-03-30 21:16:22 +0300

    comit

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private Transform[] _movePoints;
+    [SerializeField] private float _speed;
+
+    private SpriteRenderer _spriteRenderer;
+    private int _pointIndex;
+    private Vector2 _direction;
+
+    private void Start()
+    {
+        _direction = _movePoints[0].position;
+    }
+
+    private void Update()
+    {
+        Walk();
+    }
+
+    private void Walk()
+    {
+        float distanse = 0.3f;
+        float step = _speed * Time.deltaTime;
+
+        transform.position = Vector2.MoveTowards(transform.position, _direction, step);
+
+        if (Vector3.Distance(transform.position, _direction) < distanse)
+            MovementPointChose();
+    }
+
+    private void MovementPointChose()
+    {
+        _pointIndex = ++_pointIndex < _movePoints.Length ? _pointIndex : 0;
+        _direction = _movePoints[_pointIndex].position;
+
+        Flip();
+    }
+
+    private void Flip()
+    {
+        GetComponent<SpriteRenderer>().flipX = _direction.x < transform.position.x;
+    }
+}
---
+++ Assets/Scripts/SpawnerCoin.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerCoin : MonoBehaviour
+{
+    [SerializeField] private Coin _coin;
+    [SerializeField] private float _timeBetweenSpawnCoins;
+
+    private BoxCollider2D _boxCollider;
+    private float _elapserTime = 0;
+
+    private void Start()
+    {
+        CreateCoin();
+    }
+
+    private void Update()
+    {
+        _elapserTime += Time.deltaTime;
+
+        if( _coin == null && _elapserTime >= _timeBetweenSpawnCoins)
+        {
+            CreateCoin();
+        }
+    }
+
+    private void CreateCoin()
+    {
+        Instantiate(_coin);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.TryGetComponent<Player>(out Player player))
+        {
+            Destroy(_coin);
+        }
+    }
+}
