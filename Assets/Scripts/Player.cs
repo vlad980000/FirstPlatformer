168e1c0bf9509603aa36551486e307c0358d8da2
vlad980000 <vlad980000@mail.ru>
2022-04-11 19:41:44 +0300

    Comit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class Player : MonoBehaviour
-{
+{ 
     private List<Coin> _coins = new List<Coin>();
     private BoxCollider2D _boxCollider;
+
+    public int Money { get; set; }
 
     private void Start()
     {
